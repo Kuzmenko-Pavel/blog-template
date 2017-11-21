2a8db2f074dde29a8d107db8189f40813106365e
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-11-21 13:01:50 +0200

    update template

--- footer.php
+++ footer.php
@@ -22,6 +22,6 @@
     <small>&copy; 2006&nbsp;&ndash;&nbsp;<?php the_time('Y'); ?> YOTTOS</small>
 </footer>
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59a54843fcb9fac6"></script>
+<script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a1406ddabb43708"></script>
 </body>
 </html>
