23b5d737f7312489f091b29c234d5d236c812e9a
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-05-15 11:20:11 +0300

    fix

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,6 @@ if (is_page() && is_active_sidebar('sidebar-pages')) :
 elseif (is_active_sidebar('sidebar-blog')) :
     dynamic_sidebar('sidebar-blog');
 else :
-	the_widget('WP_Widget_Pages', 'title=Contents&sortby=post_modified');
+	the_widget( 'WP_Widget_Recent_Posts', 'title=_( \'Recent Comments\' )&number=10' );
 endif; ?>
 </ul>
