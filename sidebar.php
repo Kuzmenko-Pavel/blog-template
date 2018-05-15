079575b7f3d51d8436fa4045ae24e905ca891240
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-05-15 11:23:40 +0300

    fix

--- sidebar.php
+++ sidebar.php
@@ -6,5 +6,6 @@ elseif (is_active_sidebar('sidebar-blog')) :
     dynamic_sidebar('sidebar-blog');
 else :
 	the_widget( 'WP_Widget_Recent_Posts', 'title=Популярные записи&number=10' );
+	the_widget( 'WP_Widget_Tag_Cloud', 'title=Теги' );
 endif; ?>
 </ul>
