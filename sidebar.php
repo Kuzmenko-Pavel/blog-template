5ee87bc7c0eb5dd3a7a2d817ff27f8b094ee51bb
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-05-15 11:16:23 +0300

    fix

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,6 @@ if (is_page() && is_active_sidebar('sidebar-pages')) :
 elseif (is_active_sidebar('sidebar-blog')) :
     dynamic_sidebar('sidebar-blog');
 else :
-    dynamic_sidebar('sidebar-other');
+	the_widget('WP_Widget_Pages', 'title=Contents&sortby=post_modified', 'before_title=<h3>&after_title=</h3>');
 endif; ?>
 </ul>
