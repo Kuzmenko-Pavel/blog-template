320398e05192e68e34b220e6c7eab4e7723309d5
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-05-16 14:05:13 +0300

    fix

--- 404.php
+++ 404.php
@@ -2,7 +2,12 @@
 	<section class="section-main">
 		<div class="container">
 			<main class="main">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php
+				$popularpost = new WP_Query( array( 'posts_per_page' => 4,
+                                                    'meta_key' => 'post_views_count',
+                                                    'orderby' => 'meta_value_num',
+                                                    'order' => 'DESC'  ) );
+                if ( $popularpost->have_posts() ) : while ( $popularpost->have_posts() ) : $popularpost->the_post(); ?>
 					<?php get_template_part( '/includes/front/short_article'); ?>
 				<?php endwhile; ?>
 				<?php endif; ?>
