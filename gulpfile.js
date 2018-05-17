e79af28fca89acf0f788ad3d408bac75781a1d49
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2018-05-17 15:11:36 +0300

    css clean

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,22 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var rollup = require('gulp-rollup');
+var cleanCSS = require('gulp-clean-css');
+var merge = require('merge-stream');
+var rename = require('gulp-rename');
+var stripCssComments = require('gulp-strip-css-comments');
 
 gulp.task('sass', function () {
     return gulp.src('./scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
+        .pipe(stripCssComments())
         .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'))
+        .pipe(stripCssComments({preserve: false}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./'));
 });
 
